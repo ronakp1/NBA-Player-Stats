b778ac74f64bbc0ce49e705be472d3dc483fb8c6
Ronak Patel <ronakpatel@live.co.uk>
2021-04-14 11:36:03 +0100

    changes

--- nba.js
+++ nba.js
@@ -1,39 +1,24 @@
-//const api_url = 'https://www.balldontlie.io/api/v1/season_averages?season=2018&player_ids[]=237';
-const api_url = 'https://www.balldontlie.io/api/v1/players?search=';
-const api_url1 = 'https://www.balldontlie.io/api/v1/season_averages?player_ids[]=';
 const names = document.querySelector('[data-search]');
 const submit = document.querySelector('[data-submit]');
-const divs = document.getElementsByClassName('stats');
-const test1 = document.getElementById('test1');
 
 submit.addEventListener('click', button => {
     const nameSearch = names.value;
-    const newurl = api_url + nameSearch;
-    console.log(nameSearch);
-    console.log(newurl);
+    const newurl = `https://www.balldontlie.io/api/v1/players?search=${nameSearch};`
     getPlayerInfo(newurl);
+    const bar = document.getElementById('results');
+    bar.classList.add('hide');
 })
 
 async function getSeasonAverages(player_id) {
-    var html1 = ''
     try {
-        console.log(player_id)
-        const seasonsURL = 'https://www.balldontlie.io/api/v1/season_averages?player_ids[]=' + player_id;
-        console.log(seasonsURL)
+        const seasonsURL = `https://www.balldontlie.io/api/v1/season_averages?player_ids[]=${player_id}`;
         const response = await fetch(seasonsURL)
         const { data } = await response.json();
-        document.querySelector('#gp > h3').innerHTML = data[0].games_played;
-        //document.querySelector('#player_id > h3').innerHTML = data[0].player_id;
+        //const docSelec = document.querySelector;
+
+       // document.querySelector('gp > h3').innerHTML = data[0].games_played;
         document.querySelector('#season > h3').innerHTML = data[0].season;
         document.querySelector('#min > h3').innerHTML = data[0].min;
-        //document.querySelector('#fgm > h3').innerHTML = data[0].fgm;
-        //document.querySelector('#fga > h3').innerHTML = data[0].fga;
-        //document.querySelector('#fg3a > h3').innerHTML = data[0].fg3a;        
-        //document.querySelector('#fg3m > h3').innerHTML = data[0].fg3m;
-        //document.querySelector('#ftm > h3').innerHTML = data[0].ftm;
-        //document.querySelector('#fta > h3').innerHTML = data[0].fta;
-       // document.querySelector('#oreb > h3').innerHTML = data[0].oreb;
-       // document.querySelector('#dreb > h3').innerHTML = data[0].dreb;
         document.querySelector('#reb > h3').innerHTML = data[0].reb;
         document.querySelector('#ast > h3').innerHTML = data[0].ast;
         document.querySelector('#stl > h3').innerHTML = data[0].stl;
@@ -44,8 +29,6 @@ async function getSeasonAverages(player_id) {
         document.querySelector('#fg_pct > h3').innerHTML = data[0].fg_pct;
         document.querySelector('#fg3_pct > h3').innerHTML = data[0].fg3_pct;
         document.querySelector('#ft_pct > h3').innerHTML = data[0].ft_pct;
-
-
     } catch (error) {
         console.log(error)
     }
@@ -54,15 +37,11 @@ async function getSeasonAverages(player_id) {
 async function getPlayerInfo(newurl) {
     try {
         const response = await fetch(newurl);
-        //console.log(response);
         const data = await response.json();
-        //console.log(data);
 
         document.getElementById('name').textContent = `${data.data[0].first_name} ${data.data[0].last_name}`;
-        //document.getElementById('surname').textContent = data.data[0].last_name;
         document.getElementById('position').textContent = `Position: ${data.data[0].position}`;
         document.getElementById('height_feet').textContent = `Height: ${data.data[0].height_feet} ${data.data[0].height_inches}`;
-        //document.getElementById('height_inches').textContent = data.data[0].height_inches;
         document.getElementById('weight_pounds').textContent = `Weight: ${data.data[0].weight_pounds}`;
         document.getElementById('full_name').textContent = `Team: ${data.data[0].team.full_name}`;
         const player_id = data.data[0].id;
@@ -72,3 +51,45 @@ async function getPlayerInfo(newurl) {
     }
 }
 
+
+const debounce = (func, delay = 1000) => {
+    let timeoutId;
+    return (...args) => {
+        if (timeoutId) {
+            clearTimeout(timeoutId);
+        }
+        timeoutId = setTimeout(() => {
+            func.apply(null, args);
+        }, delay);
+    };
+};
+
+async function getListOfPlayers() {
+    try {
+        const playerName = names.value;
+        //response.preventDefault();
+        const response = await fetch(`https://www.balldontlie.io/api/v1/players?per_page=100&search=${playerName}`);
+        const data = await response.json();
+        console.log(data.data);
+        const bar = document.getElementById('results');
+        for (let player of data.data) {
+            console.log(`${player.first_name} ${player.last_name} ${player.team.full_name}`);
+            const dropDown = document.createElement("a");
+            dropDown.classList.add('dropdown');
+
+            dropDown.innerHTML = `${player.first_name} ${player.last_name} - ${player.team.full_name}`;
+
+            dropDown.addEventListener('click', () => {
+                bar.classList.add('hide');
+                names.value = `${player.first_name} ${player.last_name}`;
+
+            })
+            bar.appendChild(dropDown);
+
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+names.addEventListener('input', debounce(getListOfPlayers, 500));
